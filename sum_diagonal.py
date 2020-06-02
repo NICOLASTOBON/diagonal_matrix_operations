83662847e95459c7277049aab06171b2e4d5e4b1
NICOLASTOBON <nicolas.tobon009@hotmail.com>
2020-06-01 22:22:45 -0500

    sum two diagonals of matrix

--- sum_diagonal.py
+++ sum_diagonal.py
@@ -2,12 +2,20 @@
 import random
 import numpy as np
 
+
 def sum_diagonal(matrix):
 
     result = 0
+    number = 0
 
     for i in range(len(matrix)):
         result += matrix[i][i] + matrix[i][-i-1]
+
+        # check the number where the diagonals are intercepted
+        if matrix[i][i] == matrix[i][-i-1]:
+            number = matrix[i][i]
+    print()
+    print(f"Number of interception: {number}")
 
     return result
 
@@ -19,7 +27,7 @@ if __name__ == "__main__":
     matrix = np.random.randint(100, size=(size_of_matrix, size_of_matrix))
     print(matrix)
 
-    #function sum diagonals matrix
+    # function sum diagonals matrix
     result = sum_diagonal(matrix)
 
     print(f"The sum of diagonals is: {result}")
